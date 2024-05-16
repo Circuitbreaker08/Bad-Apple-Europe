7268fc7e6910c82fca5c38ba138277e10b8f8ccf
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-05-15 21:36:32 -0400

    Progressed provincial detection (dysfunctional)

--- map_processor.py
+++ map_processor.py
@@ -1,17 +1,47 @@
 from PIL import Image
 
-im = Image.open("europe_source.png").convert("RGB")
+im = Image.open("europe_source.png").convert("RGB").resize((480, 360))
 
-provinces = []
+provinces: list[set] = []
+borders = []
+ocean = []
+water = (112, 145, 196)
 taken_pixels = set()
 
-for x in im.size[0]:
-    for y in im.size[1]:
+for x in range(im.size[0]):
+    for y in range(im.size[1]):
+        color = im.getpixel((x, y))
         if (x, y) in taken_pixels:
             continue
-        
+        elif (
+            color[0] <= 190 and
+            color[1] <= 190 and
+            color[2] <= 190
+        ):
+            borders.append((x, y))
+            taken_pixels.add((x, y))
+        elif (
+            color[0] >= water[0] - 5 and color[0] <= water[0] + 5 and
+            color[1] >= water[1] - 5 and color[1] <= water[1] + 5 and
+            color[2] >= water[2] - 5 and color[2] <= water[2] + 5
+        ):
+            ocean.append((x, y))
+            taken_pixels.add((x, y))
+        else:
+            merge_provinces = []
+            for province in provinces:
+                if (x + 1, y) in province or (x - 1, y) in province or (x, y + 1) in province or (x, y - 1) in province:
+                    merge_provinces.append(province)
+            if merge_provinces == []:
+                provinces.append(set(((x, y),)))
+            else:
+                new_province = set()
+                for province in merge_provinces:
+                    provinces.pop(provinces.index(province))
+                    new_province |= province
+                provinces.append(new_province)
+            taken_pixels.add((x, y))
 
-        """
-        take each pixel
-        propogate claim
-        """
+print(borders)
+print(ocean)
+print(provinces)
